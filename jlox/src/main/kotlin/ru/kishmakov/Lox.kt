6f89dcca84d25bb2ea0b8ec5ff2d7200f7883357
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2022-01-04 23:48:34 +0300

    Introduce basic parsing

--- jlox/src/main/kotlin/ru/kishmakov/Lox.kt
+++ jlox/src/main/kotlin/ru/kishmakov/Lox.kt
@@ -1,5 +1,6 @@
 package ru.kishmakov
 
+
 sealed class RunResult {
     object Ok : RunResult()
     object Error: RunResult()
@@ -9,21 +10,26 @@ class Lox {
     fun run(source: String): RunResult {
         val scanner = Scanner(source, this)
         val tokens: List<Token> = scanner.scanTokens()
+        val parser = Parser(tokens, this)
+        val expression = parser.parse()
 
         if (hadError) return RunResult.Error
 
-        // For now, just print the tokens.
-
-        // For now, just print the tokens.
-        for (token in tokens) {
-            println(token)
-        }
+        println(AstPrinter().print(expression!!))
 
         return RunResult.Ok
     }
 
     fun error(line: Int, message: String) {
         report(line, "", message)
+    }
+
+    fun error(token: Token, message: String?) {
+        if (token.type === TokenType.EOF) {
+            report(token.line, " at end", message!!)
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message!!)
+        }
     }
 
     private fun report(line: Int, where: String, message: String) {
---
+++ jlox/src/main/kotlin/ru/kishmakov/Parser.kt
@@ -0,0 +1,134 @@
+package ru.kishmakov
+
+
+internal class Parser(private val tokens: List<Token>, private val lox: Lox) {
+    private var current = 0
+
+    fun parse(): Expr? {
+        return try {
+            expression()
+        } catch (error: ParseError) {
+            null
+        }
+    }
+
+    private fun expression(): Expr {
+        return equality()
+    }
+
+    private fun equality(): Expr {
+        var expr = comparison()
+
+        while (match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+            val operator = previous()
+            val right = comparison()
+            expr = Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun comparison(): Expr {
+        var expr = term()
+        while (match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL)) {
+            val operator = previous()
+            val right = term()
+            expr = Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun term(): Expr {
+        var expr = factor()
+        while (match(TokenType.MINUS, TokenType.PLUS)) {
+            val operator = previous()
+            val right = factor()
+            expr = Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun factor(): Expr {
+        var expr: Expr = unary()
+        while (match(TokenType.SLASH, TokenType.STAR)) {
+            val operator = previous()
+            val right: Expr = unary()
+            expr = Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun unary(): Expr {
+        if (match(TokenType.BANG, TokenType.MINUS)) {
+            val operator = previous()
+            val right = unary()
+            return Unary(operator, right)
+        }
+        return primary()
+    }
+
+    private fun primary(): Expr {
+        if (match(TokenType.FALSE)) return Literal(false)
+        if (match(TokenType.TRUE)) return Literal(true)
+        if (match(TokenType.NIL)) return Literal(null)
+        if (match(TokenType.NUMBER, TokenType.STRING)) {
+            return Literal(previous().literal)
+        }
+        if (match(TokenType.LEFT_PAREN)) {
+            val expr = expression()
+            consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+            return Grouping(expr)
+        }
+
+        throw error(peek(), "Expect expression.");
+    }
+
+    private fun consume(type: TokenType, message: String): Token {
+        if (check(type)) return advance()
+        throw error(peek(), message)
+    }
+
+    private fun error(token: Token, message: String): ParseError {
+        lox.error(token, message)
+        return ParseError()
+    }
+
+    private fun synchronize() {
+        advance()
+        while (!isAtEnd()) {
+            if (previous().type === TokenType.SEMICOLON) return
+            when (peek().type) {
+                TokenType.CLASS, TokenType.FUN,
+                TokenType.VAR, TokenType.FOR,
+                TokenType.IF, TokenType.WHILE,
+                TokenType.PRINT, TokenType.RETURN -> return
+            }
+            advance()
+        }
+    }
+
+    private fun match(vararg types: TokenType): Boolean {
+        for (type in types) {
+            if (check(type)) {
+                advance()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun advance(): Token {
+        if (!isAtEnd()) current++
+        return previous()
+    }
+
+    private fun check(type: TokenType) = if (isAtEnd()) false else peek().type == type
+
+    private fun isAtEnd() = peek().type == TokenType.EOF
+
+    private fun peek() = tokens[current]
+
+    private fun previous() = tokens[current - 1]
+}
+
+private class ParseError : RuntimeException()
