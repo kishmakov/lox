c6bed8470fcf8c0e240fd597353fa05a6d846bb8
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2022-05-13 23:59:02 +0200

    Support closures

--- jlox/src/main/kotlin/ru/kishmakov/Interpreter.kt
+++ jlox/src/main/kotlin/ru/kishmakov/Interpreter.kt
@@ -145,7 +145,7 @@ class Interpreter(private val lox: Lox) :
     }
 
     override fun visitFunctionStmt(stmt: Stmt.Function) {
-        val function = LoxFunction(stmt)
+        val function = LoxFunction(stmt, environment)
         environment.define(stmt.name.lexeme, function)
     }
     
--- jlox/src/main/kotlin/ru/kishmakov/LoxFunction.kt
+++ jlox/src/main/kotlin/ru/kishmakov/LoxFunction.kt
@@ -1,10 +1,10 @@
 package ru.kishmakov
 
-class LoxFunction(private val declaration: Stmt.Function): LoxCallable {
+class LoxFunction(private val declaration: Stmt.Function, private val closure: Environment): LoxCallable {
     override val arity = declaration.params.size
 
     override fun call(interpreter: Interpreter, arguments: List<Any?>): Any? {
-        val environment = Environment(interpreter.globals)
+        val environment = Environment(closure)
         for (i in declaration.params.indices) {
             environment.define(declaration.params[i].lexeme, arguments[i])
         }
